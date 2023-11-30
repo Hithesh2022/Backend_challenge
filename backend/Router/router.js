df3d2793eb31628ef7c1741990a9dd4fefa5c99f
Hithesh2022 <Hith68616@gmail.com>
2023-11-30 13:23:01 +0530

    added user profile,post and comment routes

--- backend/Router/router.js
+++ backend/Router/router.js
@@ -4,15 +4,25 @@ const router = Router();
 
 import { isApiAuthenticated } from "../middlewares/authMiddleware.js";
 import { login } from "../controllers/login.js"
+import { Logout } from "../controllers/logout.js"
 import { signup } from "../controllers/signup.js"
 //import { verifyEmail } from "../controllers/login.js"
 import { validateLogin, validateSignup, forgetPasswordValidator } from '../validators/AuthValidators.js';
 import { validationErrorHandler } from '../middlewares/validationErrorHandler.js';
+import {Verify} from '../middlewares/Verifycookie.js';
+import {GetProfile,ChangeName,changeEmail,UploadPhoto} from '../controllers/ManageProfile.js';
+import {UploadPost,GetPost,CommentPost} from '../controllers/ManagePost.js';
 
 router.post("/login", validateLogin, validationErrorHandler, login);
-
+router.get('/view-profile',isApiAuthenticated,GetProfile)
 router.post("/signup", validateSignup, validationErrorHandler, signup);
-
+router.post('/logout',Verify,Logout)
+router.post('/change-name',isApiAuthenticated,ChangeName)
+router.post('/change-email',isApiAuthenticated,changeEmail)
+router.post('/upload-photo',isApiAuthenticated,UploadPhoto)//later look into to it also add in usermodel will do later
+router.post('/create-post',isApiAuthenticated,UploadPost)
+router.get('/view-post',isApiAuthenticated,GetPost)
+router.post('/comment/:_id',isApiAuthenticated,CommentPost)
 
 
 export default router;
